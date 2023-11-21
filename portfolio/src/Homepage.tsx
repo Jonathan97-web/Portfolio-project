7be25a39c6a7b061d36b088746f8f09e4e9951f2
Jonathan Zakrisson <zakrisson97@gmail.com>
2023-11-21 22:25:48 +0100

    Create new routing paths and css files

---
+++ portfolio/src/Homepage.tsx
@@ -0,0 +1,11 @@
+import './css/Homepage.css';
+import NavBar from './components/Navbar';
+
+function Homepage() {
+  return (
+    <NavBar />
+  );
+}
+
+export default Homepage;
+  
--- portfolio/src/index.tsx
+++ portfolio/src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import Homepage from './Homepage';
+import Error from './pages/Error'
+import Projects from './pages/Projects';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter,
+RouterProvider, } from 'react-router-dom';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Homepage />,
+    errorElement: <Error />
+  },
+  {
+    path: "/projects",
+    element: <Projects />,
+    errorElement: <Error />
+    
+  }
+
+])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
