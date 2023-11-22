5560657c437708c205558b7cb1445b589f2fee18
Jonathan Zakrisson <zakrisson97@gmail.com>
2023-11-22 12:20:15 +0100

    Add content to project page

--- portfolio/src/pages/Projects.tsx
+++ portfolio/src/pages/Projects.tsx
@@ -1,10 +1,25 @@
 import NavBar from '../components/Navbar';
+import "../css/Projects.css"
+import brainteaser from "../images/am-i-responsive-brainteaser.png"
 
 function Projects() {
   return (
     <div>
     <NavBar />
-    <p>hello</p>
+    <div className='projects'>
+      <div className='card'>
+        <div className='card-body'>
+          <img src={brainteaser} alt="My first project called brainteaser." />
+        </div>
+        <div className='card-footer mx-auto'>
+          <h2>Brainteaser is a mental health awareness page, as a user you can read about several mental diagnoses.</h2>
+        </div>
+      </div>
+      <p>Project2</p>
+      <p>Project3</p>
+      <p>Project4</p>
+      <p>Project5</p>
+    </div>
     </div>
   );
 }
